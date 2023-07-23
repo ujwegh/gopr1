861ddd6373879f3e54ce66b502870bba1b157f41
ujwegh <nikita.aleksandrov@abfprogramming.com>
2023-07-23 22:48:10 +0400

    lesson 4

--- main.go
+++ main.go
@@ -2,60 +2,55 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+	executeTemplate(w, "templates/home.gohtml")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to contact page</h1>")
+	executeTemplate(w, "templates/contact.gohtml")
 }
+
 func faqHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, "templates/faq.gohtml")
+}
+
+func galleriesHandler(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	fmt.Fprint(w, fmt.Sprintf("hi %v", id))
+}
+
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ page</h1>
-		<ul>
-			<li>
-			<b>Is there free version?</b>
-			No only paid version
-			</li>
-			<li>
-			<b>How much does it cost?</b>
-			$500
-			</li>
-			<li>
-			<b>Can i pay by credit card?</b>
-			Only by cash
-			</li>
-		</ul>`)
-}
-
-type Router struct {
-}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
-
-func main() {
-	//http.HandleFunc("/", pathHandler)
-	//http.HandleFunc("/contact", contactHandler)
-	var router Router
-	fmt.Println("arting server on :3000...")
-	err := http.ListenAndServe(":3000", router)
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
-		panic(err)
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
 	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
+}
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.Get("/galleries/{id}", galleriesHandler)
+	r.NotFound(func(writer http.ResponseWriter, request *http.Request) {
+		http.Error(writer, "Page not found", http.StatusNotFound)
+	})
+	fmt.Println("Starting server on :3000...")
+	http.ListenAndServe(":3000", r)
 }
