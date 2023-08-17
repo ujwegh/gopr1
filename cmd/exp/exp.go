f7c1a2dfa57c0e36079a7bf9e35d74e39f93a8cd
ujwegh <nikita.aleksandrov@abfprogramming.com>
2023-08-17 11:17:47 +0300

    lesson 10.7

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -41,13 +41,62 @@ func Sum(numbers ...int) int {
 	return sum
 }
 
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
 func main() {
-	db, err := sql.Open("pgx", "postgres://postgres:mysecretpassword@localhost:5432/postgres?sslmode=disable")
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "postgres",
+		Password: "mysecretpassword",
+		Database: "postgres",
+		SSLMode:  "disable",
+	}
+	db, err := sql.Open("pgx", cfg.String())
 	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Connected!")
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		name TEXT,
+		email TEXT NOT NULL
+		);
+		CREATE TABLE IF NOT EXISTS orders (
+		id SERIAL PRIMARY KEY,
+		user_id INT NOT NULL,
+		amount INT,
+		description TEXT
+		);`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Tables created.")
+
+	name := "New User"
+	email := "new@calhoun.io"
+	row := db.QueryRow(`
+		INSERT INTO users (name, email)
+		VALUES ($1, $2) RETURNING id;`, name, email)
+	var id int
+	err = row.Scan(&id)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("User created. id =", id)
 	defer db.Close()
 }
 func Join(vals ...string) string {
