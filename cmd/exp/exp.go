d46e4a03db560311815e2404f9e23906d958b029
ujwegh <nikita.aleksandrov@abfprogramming.com>
2023-08-19 14:51:55 +0300

    lesson 12.4

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"errors"
 	"fmt"
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"gopr/models"
 	"strings"
 )
 
@@ -41,20 +41,6 @@ func Sum(numbers ...int) int {
 	return sum
 }
 
-type PostgresConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Database string
-	SSLMode  string
-}
-
-func (cfg PostgresConfig) String() string {
-	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
-}
-
 type Order struct {
 	ID          int
 	UserID      int
@@ -63,101 +49,25 @@ type Order struct {
 }
 
 func main() {
-	cfg := PostgresConfig{
-		Host:     "localhost",
-		Port:     "5432",
-		User:     "postgres",
-		Password: "mysecretpassword",
-		Database: "postgres",
-		SSLMode:  "disable",
+	cfg := models.DefaultPostgresConfig()
+	db, err := models.Open(cfg)
+	if err != nil {
+		panic(err)
 	}
-	db, err := sql.Open("pgx", cfg.String())
+	defer db.Close()
 	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
-
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
-		id SERIAL PRIMARY KEY,
-		name TEXT,
-		email TEXT NOT NULL
-		);
-		CREATE TABLE IF NOT EXISTS orders (
-		id SERIAL PRIMARY KEY,
-		user_id INT NOT NULL,
-		amount INT,
-		description TEXT
-		);`)
+	fmt.Println("Connected!")
+	us := models.UserService{
+		DB: db,
+	}
+	user, err := us.Create("bob4@bob.com", "bob123")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Tables created.")
-
-	//name := "New User"
-	//email := "new@calhoun.io"
-	//row := db.QueryRow(`
-	//	INSERT INTO users (name, email)
-	//	VALUES ($1, $2) RETURNING id;`, name, email)
-	//var id int
-	//err = row.Scan(&id)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Println("User created. id =", id)
-	//userID := 3 // Pick an ID that exists in your DB
-	//for i := 0; i < 5; i++ {
-	//	amount := i * 100
-	//	desc := fmt.Sprintf("Fake order #%d", i)
-	//	_, err := db.Exec(`
-	//		INSERT INTO orders(user_id, amount, description)
-	//		VALUES($1, $2, $3)`, userID, amount, desc)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//}
-
-	//id := 5
-	//row := db.QueryRow(`
-	//	SELECT id, name, email
-	//	FROM users
-	//	WHERE id=$1;`, id)
-	//var name, email string
-	//err = row.Scan(&id, &name, &email)
-	//if errors.Is(err, sql.ErrNoRows) {
-	//	fmt.Println("Error, no rows!")
-	//}
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Printf("User information: id:%d name=%s, email=%s\n", id, name, email)
-
-	var orders []Order
-	userID := 3 // Use the same ID you used in the previous lesson
-	rows, err := db.Query(`
-		SELECT id, amount, description
-		FROM orders
-		WHERE user_id=$1`, userID)
-	if err != nil {
-		panic(err)
-	}
-
-	for rows.Next() {
-		var order Order
-		order.UserID = userID
-		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
-		if err != nil {
-			panic(err)
-		}
-		orders = append(orders, order)
-	}
-	err = rows.Err()
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-	fmt.Println("Orders:", orders)
-
-	defer db.Close()
+	fmt.Println(user)
 }
 func Join(vals ...string) string {
 	var sb strings.Builder
---
+++ models/user.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"strings"
+)
+
+type User struct {
+	ID           int
+	Email        string
+	PasswordHash string
+}
+type NewUser struct {
+	Email    string
+	Password string
+}
+
+type UserService struct {
+	DB *sql.DB
+}
+
+func (us *UserService) Create(email, password string) (*User, error) {
+	email = strings.ToLower(email)
+	passwordHash := GeneratePasswordHash(password)
+	user := User{
+		Email:        email,
+		PasswordHash: passwordHash,
+	}
+	row := us.DB.QueryRow(`
+		INSERT INTO users (email, password_hash)
+		VALUES ($1, $2) returning id`, user.Email, user.PasswordHash)
+	err := row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+	return &user, nil
+}
+
+func GeneratePasswordHash(password string) string {
+	hashedBytes, err := bcrypt.GenerateFromPassword(
+		[]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(fmt.Errorf("generate hash error: %w", err))
+	}
+	return string(hashedBytes)
+}
+
+type UserCreator interface {
+	Create(email, password string) (*User, error)
+}
