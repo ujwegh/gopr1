7553f7ba6f46a27403a54d8c73dc643841256faa
ujwegh <nikita.aleksandrov@abfprogramming.com>
2023-08-21 23:05:21 +0300

    lesson 13.8.2

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gorilla/csrf"
 	"gopr/controllers"
 	"gopr/models"
 	"gopr/templates"
 	"gopr/views"
 	"log"
 	"net/http"
+	"time"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +40,14 @@ func executeTemplate(w http.ResponseWriter, filepath string) {
 		return
 	}
 	tpl.Execute(w, nil)
+}
+
+func TimerMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		h(w, r)
+		fmt.Println("Request time:", time.Since(start))
+	}
 }
 
 func main() {
@@ -72,7 +82,7 @@ func main() {
 	usersC.Templates.New = views.MustParse(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	usersC.Templates.SignIn = views.MustParse(views.ParseFS(
 		templates.FS, "signin.gohtml", "tailwind.gohtml"))
-	r.Get("/signup", usersC.New)
+	r.Get("/signup", TimerMiddleware(usersC.New))
 	r.Post("/signup", usersC.Create)
 	r.Get("/signin", usersC.SignIn)
 	r.Post("/signin", usersC.ProcessSignIn)
@@ -83,6 +93,13 @@ func main() {
 		http.Error(writer, "Page not found", http.StatusNotFound)
 	})
 
+	csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
+	csrfMw := csrf.Protect(
+		[]byte(csrfKey),
+		// TODO: Fix this before deploying
+		csrf.Secure(false),
+	)
+
 	fmt.Println("Starting server on :3000...")
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3000", csrfMw(r))
 }
