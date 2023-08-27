3fb6bbf1f7a5f19324129f67a6418c90d83e8b63
ujwegh <nikita.aleksandrov@abfprogramming.com>
2023-08-27 15:35:02 +0300

    lesson 15.9

--- models/session.go
+++ models/session.go
@@ -33,14 +33,13 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		Token:     token,
 		TokenHash: tokenHash,
 	}
-	row := ss.DB.QueryRow(`UPDATE sessions SET token_hash = $2 WHERE user_id = $1 RETURNING id;`,
-		session.UserID, session.TokenHash)
+	row := ss.DB.QueryRow(`
+		INSERT INTO sessions (user_id, token_hash)
+		VALUES ($1, $2) ON CONFLICT (user_id) DO
+		UPDATE
+		SET token_hash = $2
+		RETURNING id;`, session.UserID, session.TokenHash)
 	err = row.Scan(&session.ID)
-	if err == sql.ErrNoRows {
-		row = ss.DB.QueryRow(`INSERT INTO sessions (user_id, token_hash) VALUES ($1, $2) RETURNING id;`,
-			session.UserID, session.TokenHash)
-		err = row.Scan(&session.ID)
-	}
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
