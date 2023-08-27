17a83f4dcda5530d508e3b6ccdc3cc52820f0266
ujwegh <nikita.aleksandrov@abfprogramming.com>
2023-08-27 18:35:58 +0300

    lesson 17.1

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	_ "github.com/jackc/pgx/v4/stdlib"
-	"gopr/models"
 	"strings"
 )
 
@@ -49,25 +49,10 @@ type Order struct {
 }
 
 func main() {
-	cfg := models.DefaultPostgresConfig()
-	db, err := models.Open(cfg)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Connected!")
-	us := models.UserService{
-		DB: db,
-	}
-	user, err := us.Create("bob4@bob.com", "bob123")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(user)
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "favorite-color", "blue")
+	value := ctx.Value("favorite-color")
+	fmt.Println(value)
 }
 func Join(vals ...string) string {
 	var sb strings.Builder
