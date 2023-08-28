5cad290247831f38c0e1221a4dea303549e1074d
ujwegh <nikita.aleksandrov@abfprogramming.com>
2023-08-28 16:51:07 +0300

    lesson 18.7

--- models/email.go
+++ models/email.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/go-mail/mail/v2"
+import (
+	"fmt"
+	"github.com/go-mail/mail/v2"
+)
 
 const (
 	// DefaultSender is the default email address to send emails from.
@@ -23,4 +26,48 @@ func NewEmailService(config SMTPConfig) *EmailService {
 		dialer: mail.NewDialer(config.Host, config.Port, config.Username, config.Password),
 	}
 	return &es
+}
+
+type Email struct {
+	From      string
+	To        string
+	Subject   string
+	Plaintext string
+	HTML      string
+}
+
+func (es *EmailService) Send(email Email) error {
+	msg := mail.NewMessage()
+	msg.SetHeader("To", email.To)
+	// TODO: Set the From field to a default value if it is not
+	// set in the Email
+	msg.SetHeader("From", getFrom(email, es))
+	msg.SetHeader("Subject", email.Subject)
+	switch {
+	case email.Plaintext != "" && email.HTML != "":
+		msg.SetBody("text/plain", email.Plaintext)
+		msg.AddAlternative("text/html", email.HTML)
+	case email.Plaintext != "":
+		msg.SetBody("text/plain", email.Plaintext)
+	case email.HTML != "":
+		msg.SetBody("text/html", email.HTML)
+	}
+	err := es.dialer.DialAndSend(msg)
+	if err != nil {
+		return fmt.Errorf("send: %w", err)
+	}
+	return nil
+}
+
+func getFrom(email Email, es *EmailService) string {
+	var from string
+	switch {
+	case email.From != "":
+		from = email.From
+	case es.DefaultSender != "":
+		from = es.DefaultSender
+	default:
+		from = DefaultSender
+	}
+	return from
 }
