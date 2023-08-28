d7cd2ed04d4f0ba3dd564290fc09b2d660119d64
ujwegh <nikita.aleksandrov@abfprogramming.com>
2023-08-28 17:23:22 +0300

    lesson 18.9

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -3,9 +3,12 @@ package main
 import (
 	"errors"
 	"fmt"
-	"github.com/go-mail/mail/v2"
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/joho/godotenv"
+	"gopr/models"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -57,24 +60,29 @@ const (
 )
 
 func main() {
-	from := "test@ujwegh.com"
-	to := "nik29200018@gmail.com"
-	subject := "This is a test email"
-	plaintext := "This is the body of the email"
-	html := `<h1>Hello there buddy!</h1><p>This is the email</p><p>Hope you enjoy it</p>`
-	msg := mail.NewMessage()
-	msg.SetHeader("To", to)
-	msg.SetHeader("From", from)
-	msg.SetHeader("Subject", subject)
-	msg.SetBody("text/plain", plaintext)
-	msg.AddAlternative("text/html", html)
-	msg.WriteTo(os.Stdout)
-	dialer := mail.NewDialer(host, port, username, password)
-	err := dialer.DialAndSend(msg)
+	err := godotenv.Load()
 	if err != nil {
-		// TODO: Handle the error correctly
+		log.Fatal("Error loading .env file")
+	}
+	host := os.Getenv("SMTP_HOST")
+	portStr := os.Getenv("SMTP_PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
 		panic(err)
 	}
+	username := os.Getenv("SMTP_USERNAME")
+	password := os.Getenv("SMTP_PASSWORD")
+	es := models.NewEmailService(models.SMTPConfig{
+		Host:     host,
+		Port:     port,
+		Username: username,
+		Password: password,
+	})
+	err = es.ForgotPassword("jon@calhoun.io", "https://lenslocked.com/reset-pw?token=abc123")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Email sent")
 }
 func Join(vals ...string) string {
 	var sb strings.Builder
