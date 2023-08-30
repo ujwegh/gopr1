846b53791773aae20378f845e991389cf0d47478
ujwegh <nikita.aleksandrov@abfprogramming.com>
2023-08-30 12:58:34 +0300

    lesson 19.11

--- models/password_reset.go
+++ models/password_reset.go
@@ -1,8 +1,12 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"gopr/rand"
+	"strings"
 	"time"
 )
 
@@ -25,11 +29,51 @@ type PasswordResetService struct {
 	Duration      time.Duration
 }
 
-func (service *PasswordResetService) Create(email string) (*PasswordReset, error) {
-	return nil, fmt.Errorf("TODO: Implement PasswordResetService.Create")
+func (prs *PasswordResetService) Create(email string) (*PasswordReset, error) {
+	// Verify we have a valid email address for a user
+	email = strings.ToLower(email)
+	var userID int
+	row := prs.DB.QueryRow(`SELECT id FROM users WHERE email = $1;`, email)
+	err := row.Scan(&userID)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	// Build the PasswordReset
+	bytesPerToken := prs.BytesPerToken
+	if bytesPerToken == 0 {
+		bytesPerToken = MinBytesPerToken
+	}
+	token, err := rand.String(bytesPerToken)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	duration := prs.Duration
+	if duration == 0 {
+		duration = DefaultResetDuration
+	}
+	pwReset := PasswordReset{
+		UserID:    userID,
+		Token:     token,
+		TokenHash: prs.hash(token),
+		ExpiresAt: time.Now().Add(duration),
+	}
+	// Insert the PasswordReset into the DB
+	row = prs.DB.QueryRow(`INSERT INTO password_resets (user_id, token_hash, expires_at)
+		VALUES ($1, $2, $3) ON CONFLICT (user_id) DO UPDATE
+		SET token_hash = $2, expires_at = $3
+		RETURNING id;`, pwReset.UserID, pwReset.TokenHash, pwReset.ExpiresAt)
+	err = row.Scan(&pwReset.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	return &pwReset, nil
 }
 
-// We are going to consume a token and return the user associated with it, or return an error if the token is invalid or expired.
-func (service *PasswordResetService) Consume(token string) (*User, error) {
+func (prs *PasswordResetService) Consume(token string) (*User, error) {
 	return nil, fmt.Errorf("TODO: Implement PasswordResetService.Consume")
+}
+
+func (prs *PasswordResetService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
